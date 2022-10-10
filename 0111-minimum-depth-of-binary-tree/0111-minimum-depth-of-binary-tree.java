7f9c418f93a4ad26af8815637209948bebaa6c33
Subodh Thakur <111262546+subodhthakur001@users.noreply.github.com>
2022-10-10 22:14:09 +0530

    Time: 15 ms (38.02%), Space: 93.2 MB (47.73%) - LeetHub

---
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/*
+T.C. -> O(N)
+S.C. -> O(1)
+*/
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root==null)return 0;
+        int lh = minDepth(root.left);
+        int rh = minDepth(root.right);
+        if(root.left== null && root.right!=null)
+            return 1 + rh;
+        if(root.right==null && root.left!=null)
+            return 1 +lh;
+        
+        return Math.min(lh,rh)+1;
+    }
+}
