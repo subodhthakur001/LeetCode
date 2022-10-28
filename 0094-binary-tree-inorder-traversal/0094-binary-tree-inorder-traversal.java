8446da05a5be29d6a9f050a16bcee688cda32f39
Subodh Thakur <111262546+subodhthakur001@users.noreply.github.com>
2022-10-28 18:55:59 +0530

    Time: 1 ms (46.64%), Space: 42.7 MB (12.59%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -13,7 +13,8 @@
  *     }
  * }
  */
-class Solution {
+/*RECURSIVE METHOD*/
+/*class Solution {
     public void inorder(TreeNode root, List<Integer> ans){
         if(root==null)return;
         else{
@@ -25,6 +26,29 @@ class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         inorder(root,res);
+        return res;
+    }
+}
+/*ITERATIVE METHOD*/
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root){
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode node = root;
+        while(true){
+            if(node!=null){
+                st.push(node);
+                node = node.left;
+            }
+            else{
+                if(st.isEmpty()){
+                    break;
+                }
+                node = st.pop();
+                res.add(node.val);
+                node = node.right;
+            }
+        }
         return res;
     }
 }
